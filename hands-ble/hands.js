d9a322cd35ba3c699ddef6c92562a8c467361ee1
ferryzhou <ferryzhou@gmail.com>
2024-06-20 17:28:51 -0700

    log more.

--- hands-ble/hands.js
+++ hands-ble/hands.js
@@ -99,7 +99,8 @@ function onResults(results) {
         }
         if (results.multiHandLandmarks.length > 0) {
             console.log(results.multiHandLandmarks)
-            let point = results.multiHandLandmarks[0].from
+            console.log(results.multiHandLandmarks[0])
+            let point = results.multiHandLandmarks[0]
             let data = formatAndTrim(point.x * 100) + formatAndTrim(point.y * 100) + formatAndTrim(point.z * 100)
             console.log(point.x)
             console.log(point.y)
