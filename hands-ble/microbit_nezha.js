b6fd5d82a16069098172ba4a00e3eea4282b1aaf
ferryzhou <ferryzhou@gmail.com>
2024-06-22 21:08:27 -0700

    Add microbit code to follow the direction of hand. If hand is at right, turn right. If hand is at left, turn left. tested and works.

--- hands-ble/microbit_nezha.js
+++ hands-ble/microbit_nezha.js
@@ -1,15 +1,58 @@
+function moveForward () {
+    neZha.setMotorSpeed(neZha.MotorList.M1, 10)
+    neZha.setMotorSpeed(neZha.MotorList.M4, 10)
+}
 bluetooth.onBluetoothConnected(function () {
-    basic.showIcon(IconNames.Happy)
+    basic.showIcon(IconNames.Yes)
 })
 bluetooth.onBluetoothDisconnected(function () {
     basic.showIcon(IconNames.No)
+})
+input.onButtonPressed(Button.A, function () {
+    bluetooth.uartWriteString("A")
 })
 bluetooth.onUartDataReceived(serial.delimiters(Delimiters.NewLine), function () {
     receivedString = bluetooth.uartReadUntil(serial.delimiters(Delimiters.NewLine))
     x = parseFloat(receivedString.substr(0, 2))
     y = parseFloat(receivedString.substr(2, 2))
     z = parseFloat(receivedString.substr(4, 2))
+    if (x == 0) {
+        stop()
+    } else if (x > 60) {
+        rotateRight()
+        basic.pause(10)
+        stop()
+    } else if (x <= 40) {
+        rotateLeft()
+        basic.pause(10)
+        stop()
+    } else { // stop if it's in the middle
+        stop()
+    }
+    if (receivedString == "P") {
+        basic.showIcon(IconNames.Heart)
+        basic.pause(100)
+        basic.showIcon(IconNames.Yes)
+    }
 })
+input.onButtonPressed(Button.B, function () {
+    bluetooth.uartWriteString("B")
+})
+input.onGesture(Gesture.Shake, function () {
+    bluetooth.uartWriteString("S")
+})
+function rotateRight () {
+    neZha.setMotorSpeed(neZha.MotorList.M1, 10)
+    neZha.setMotorSpeed(neZha.MotorList.M4, -10)
+}
+function rotateLeft() {
+    neZha.setMotorSpeed(neZha.MotorList.M1, -10)
+    neZha.setMotorSpeed(neZha.MotorList.M4, 10)
+}
+function stop() {
+    neZha.setMotorSpeed(neZha.MotorList.M1, 0)
+    neZha.setMotorSpeed(neZha.MotorList.M4, 0)
+}
 let z = 0
 let y = 0
 let x = 0
