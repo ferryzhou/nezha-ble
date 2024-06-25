d921b278c58c8f5a0696a934c7352ecd825ddb16
ferryzhou <ferryzhou@gmail.com>
2024-06-24 21:04:08 -0700

    Update microbit robot action: shoot when target is in the middle. Tested and works!

--- hands-ble/microbit_nezha.js
+++ hands-ble/microbit_nezha.js
@@ -18,16 +18,17 @@ bluetooth.onUartDataReceived(serial.delimiters(Delimiters.NewLine), function () 
     z = parseFloat(receivedString.substr(4, 2))
     if (x == 0) {
         stop()
-    } else if (x > 60) {
+    } else if (x > 55) {
         rotateRight()
         basic.pause(10)
         stop()
-    } else if (x <= 40) {
+    } else if (x <= 45) {
         rotateLeft()
         basic.pause(10)
         stop()
     } else { // stop if it's in the middle
         stop()
+        shoot()
     }
     if (receivedString == "P") {
         basic.showIcon(IconNames.Heart)
@@ -53,9 +54,18 @@ function stop() {
     neZha.setMotorSpeed(neZha.MotorList.M1, 0)
     neZha.setMotorSpeed(neZha.MotorList.M4, 0)
 }
+function shoot() {
+    neZha.setServoAngel(neZha.ServoTypeList._180, neZha.ServoList.S4, 135)
+    basic.pause(100)
+    resetServo()
+}
+function resetServo() {
+    neZha.setServoAngel(neZha.ServoTypeList._180, neZha.ServoList.S4, 90)
+}
 let z = 0
 let y = 0
 let x = 0
 let receivedString = ""
 bluetooth.startUartService()
 basic.showIcon(IconNames.Square)
+resetServo()
